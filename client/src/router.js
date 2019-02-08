b7bbbf089cf93a39df255a3ffca7c643c3b5894a
Kaminderpal Singh <kaminderpal@livebarn.com>
2019-02-08 12:19:53 -0500

    add login component in react router

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import React, { Component,Fragment } from 'react'
 import { Route,Switch } from 'react-router-dom'
 import Home from './Containers/Home/home'
 import Register from './Components/Register/register'
+import  Login from './Components/Login/login';
 
 export class Router extends Component {
   render() {
@@ -10,6 +11,7 @@ export class Router extends Component {
            <Switch>
                 <Route path="/" exact component={Home}  />
                 <Route path="/register" exact component={Register}  />
+                <Route path="/login" exact component={Login}  />
            </Switch>
       </Fragment>
     )
