4095548e5a88a91e7b59f94c10dbda86eb4a7290
kaminderpal <kaminderpal@livebarn.com>
2019-02-10 14:28:01 -0500

    init chat conatiner

---
+++ client/src/Containers/Chat/chat.js
@@ -0,0 +1,13 @@
+import React, { Component,Fragment } from 'react'
+
+export class Chat extends Component {
+  render() {
+    return (
+      <Fragment>
+           <h1>Chat Container</h1>
+      </Fragment>
+    )
+  }
+}
+
+export default Chat
