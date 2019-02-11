d125f1799d91dd7a3f98225d66de0c8c4d902634
Kaminderpal Singh <kaminderpal@livebarn.com>
2019-02-11 13:55:14 -0500

    update routes with lazing loading

--- client/src/router.js
+++ client/src/router.js
@@ -1,22 +1,25 @@
-import React, { Component,Fragment } from 'react'
+import React, { Component,Fragment,Suspense,lazy } from 'react'
 import { Route,Switch } from 'react-router-dom'
-import Home from './Containers/Home/home'
-import Register from './Components/Register/register'
-import  Login from './Components/Login/login';
-import ForgotPwd from './Components/ForgotPassword/forgotPwd';
-import ResetPwd from './Components/ResetPassword/resetPwd';
+
+const Home = lazy(() => import('./Containers/Home/home') );
+const Login = lazy(() => import('./Components/Login/login') );
+const Register = lazy(() => import('./Components/Register/register') );
+const ForgotPwd = lazy(() => import('./Components/ForgotPassword/forgotPwd') );
+const ResetPwd = lazy(() => import('./Components/ResetPassword/resetPwd') );
 
 export class Router extends Component {
   render() {
     return (
       <Fragment>
+          <Suspense fallback={<div>Loading...</div>}>
            <Switch>
-                <Route path="/" exact component={Home}  />
-                <Route path="/register" exact component={Register}  />
-                <Route path="/login" exact component={Login}  />
-                <Route path="/forgotpassword" exact component={ForgotPwd}  />
-                <Route path="/resetpassword/:email/:token" exact component={ResetPwd}  />
+                <Route path="/" exact render={()=><Home/>}  />
+                <Route path="/register" exact render={()=><Register/>}  />
+                <Route path="/login" exact render={()=><Login/>} />
+                <Route path="/forgotpassword" exact render={()=><ForgotPwd/>}  />
+                <Route path="/resetpassword/:email/:token" exact render={()=><ResetPwd/>} />
            </Switch>
+           </Suspense>
       </Fragment>
     )
   }
